01ff3d921f7d1f3b47fea35338a982317cb07e94
leekester <58551952+leekester@users.noreply.github.com>
2023-03-13 11:40:21 +0000

    Including AKS cluster deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,32 @@
+$resourceGroup = "rg-aks"
+$location = "uksouth"
+$clusterName = "aks"
+$nodesize = "Standard_B4ms"
+$nodeCount = "1"
+$subscriptionId = (az account show --query id --output tsv)
+
+# Create AKS cluster
+Write-Host "Creating AKS cluster $clusterName in resource group $resourceGroup" -ForegroundColor Yellow
+az group create `
+  --name $resourceGroup `
+  --location $location
+az aks create `
+  --resource-group $resourceGroup `
+  --name $clusterName `
+  --node-vm-size $nodesize `
+  --node-count $nodeCount `
+  --enable-blob-driver `
+  --network-plugin azure `
+  --network-plugin-mode overlay `
+  --pod-cidr 192.168.0.0/16 `
+  --zones 1 `
+  --generate-ssh-keys
+
+# Retrieve AKS admin credentials
+Write-Host "Retrieving AKS credentials" -ForegroundColor Yellow
+az aks get-credentials --name $clusterName --resource-group $resourceGroup --overwrite-existing
+
+# Deploy Kubecost
 helm repo add kubecost https://kubecost.github.io/cost-analyzer/
 helm repo update
 helm upgrade --install kubecost kubecost/cost-analyzer --namespace kubecost --create-namespace
